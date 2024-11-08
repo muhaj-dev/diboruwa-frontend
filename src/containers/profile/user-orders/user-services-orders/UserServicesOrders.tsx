450fd4ca3eade543e95638e724ef27ca9fbb4f5c
muhammod1 <muhammodholawale@gmail.com>
2024-11-08 01:45:33 +0100

    service Tracking

--- src/containers/profile/user-orders/user-services-orders/UserServicesOrders.tsx
+++ src/containers/profile/user-orders/user-services-orders/UserServicesOrders.tsx
@@ -1,11 +1,5 @@
 import React from 'react'
-import { OrderedServices1 } from './ordered-service-cards/OrderedServices1'
-import { OrderedServices2 } from './ordered-service-cards/OrderedServices2'
-import { OrderedServices3 } from './ordered-service-cards/OrderedServices3'
-import { OrderedServices4 } from './ordered-service-cards/OrderedServices4'
-import { OrderedServices5 } from './ordered-service-cards/OrderedServices5'
-import { OrderedServices6 } from './ordered-service-cards/OrderedServices6'
-import { OrderedServices7 } from './ordered-service-cards/OrderedServices7'
+import { OrderedServices } from './ordered-service-cards/OrderedServices'
 import './user-services-orders.css'
 
 
@@ -17,13 +11,7 @@ export const UserServicesOrders = () => {
         <button className="services_btn">Rendered</button>
       </div>
       <div className="user_card_overflow">
-        <OrderedServices1 />
-        <OrderedServices2 />
-        <OrderedServices3 />
-        <OrderedServices4 />
-        <OrderedServices5 />
-        <OrderedServices6 />
-        <OrderedServices7 />
+        <OrderedServices />
       </div>
     </>
   );
